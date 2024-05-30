869aed5eb0dcbc2ea2e2240aca479f5551c7fba7
Sergey Makarov <spek94@yandex.ru>
2024-05-30 16:28:09 +0500

    Поправил Code Style чтобы он соотвествовал тестам на GitHub

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
