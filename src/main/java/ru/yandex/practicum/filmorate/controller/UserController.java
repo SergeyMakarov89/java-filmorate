916e12b14cc9109e373e92d1ccaf6caf13c0ece2
Sergey Makarov <spek94@yandex.ru>
2024-05-30 14:10:11 +0500

    Поправил HttpStatus'ы чтобы они соотвествовали тестам на GitHub

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -23,52 +23,44 @@ public class UserController {
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public Collection<User> findAll() {
         return userService.findAll();
     }
 
     @GetMapping("/{userId}")
-    @ResponseStatus(HttpStatus.OK)
     public User findUserById(@PathVariable Long userId) {
         return userService.findUserById(userId);
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public User create(@Valid @RequestBody(required = true) User user) {
         return userService.create(user);
     }
 
 
     @PutMapping
-    @ResponseStatus(HttpStatus.ACCEPTED)
     public User update(@Valid @RequestBody(required = true) User newUser) {
         return userService.update(newUser);
     }
 
     @PutMapping("/{userId}/friends/{friendId}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
     public Collection<User> addFriendToUser(@PathVariable Long userId,
                                 @PathVariable Long friendId) {
         return userService.addFriendToUser(userId, friendId);
     }
 
     @DeleteMapping("/{userId}/friends/{friendId}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
     public Collection<User> deleteFriend(@PathVariable Long userId,
                                 @PathVariable Long friendId) {
         return userService.deleteFriend(userId, friendId);
     }
 
     @GetMapping("/{userId}/friends")
-    @ResponseStatus(HttpStatus.OK)
     public Collection<User> getFriends(@PathVariable Long userId) {
         return userService.getFriends(userId);
     }
 
     @GetMapping("/{userId}/friends/common/{friendId}")
-    @ResponseStatus(HttpStatus.OK)
     public Collection<User> getCommonFriends(@PathVariable Long userId,
                                          @PathVariable Long friendId) {
         return userService.getCommonFriends(userId, friendId);
