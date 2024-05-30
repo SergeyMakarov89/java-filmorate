ac62a26f7a19d969ea1189d53315d9a94d26ae1f
Sergey Makarov <spek94@yandex.ru>
2024-05-30 14:10:02 +0500

    Поправил HttpStatus'ы чтобы они соотвествовали тестам на GitHub

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -23,45 +23,38 @@ public class FilmController {
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public Collection<Film> findAll() {
         return filmService.findAll();
     }
 
     @GetMapping("/{filmId}")
-    @ResponseStatus(HttpStatus.OK)
     public Film findFilmById(@PathVariable Long filmId) {
         return filmService.findFilmById(filmId);
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public Film create(@Valid @RequestBody(required = true) Film film) {
         return filmService.create(film);
     }
 
     @PutMapping
-    @ResponseStatus(HttpStatus.ACCEPTED)
     public Film update(@Valid @RequestBody(required = true) Film newFilm) {
         return filmService.update(newFilm);
     }
 
     @PutMapping("/{filmId}/like/{userId}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
     public Film addLikeToFilm(@PathVariable Long filmId,
                               @PathVariable Long userId) {
         return filmService.addLikeToFilm(filmId, userId);
     }
 
     @DeleteMapping("/{filmId}/like/{userId}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
     public Film deleteLikeFromFilm(@PathVariable Long filmId,
                               @PathVariable Long userId) {
         return filmService.deleteLikeFromFilm(filmId, userId);
     }
 
     @GetMapping("/popular")
-    @ResponseStatus(HttpStatus.OK)
     public Collection<Film> getPopularFilms(@RequestParam(defaultValue = "10", required = false) Long count) {
         return filmService.getPopularFilms(count);
     }
